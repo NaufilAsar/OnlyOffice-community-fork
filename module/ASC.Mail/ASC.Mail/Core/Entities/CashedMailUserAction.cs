885fa243a3f5a64cc98a0def21f098ef93166935
Name <Name@example.com>
2023-03-09 07:32:04 -0800

    Add project files.

---
+++ module/ASC.Mail/ASC.Mail/Core/Entities/CashedMailUserAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ASC.Mail.Core.Entities
+{
+    public class CashedMailUserAction
+    {
+        public string UserName { get; set; }
+        public int Tenant { get; set; }
+        public List<int> Uds { get; set; }
+        public MailUserAction Action { get; set; }
+        public int Destination { get; set; }
+        public string Data { get; set; }
+        public uint? UserFolderId  { get; set; }
+    }
+
+    public enum MailUserAction
+    {
+        Nothing,
+        SetAsRead,
+        SetAsUnread,
+        SetAsImportant,
+        SetAsNotImpotant,
+        SetAsDeleted,
+        StartImapClient,
+        MoveTo,
+        ReceiptStatusChanged,
+        Restore,
+        CreateFolder
+    }
+}
